93bb94d2c8790091b302e4c94b5b52ddeeafa00a
zhairil <45011839+zhairil@users.noreply.github.com>
2020-01-17 14:11:59 -0800

    Add files via upload

---
+++ stranger_things_theme.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+use_synth :dsaw
+live_loop :stranger_things do
+  play :C4
+  sleep 0.5
+  play :E4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :G5
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :E4
+  sleep 0.5
+end
